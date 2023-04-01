fd133a7c6ffaded439de5d20fa03353dab938d58
Rake Wangsa <rakekw28@gmail.com>
2023-04-02 01:14:21 +0700

    commit 24

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -356,7 +356,7 @@ class DashboardController extends Controller
         ->select('no_antrian', 'no_ppk', 'tanggal_antrian', 'email', 'status')
         ->get();
         $sisaCS = count($antrisisaCS);
-        
+
         return view('dashboard.operator.cs', [
             'title' => 'Dashboard',
             'active' => 'operator',
@@ -369,9 +369,155 @@ class DashboardController extends Controller
 
     public function dash_admin()
     {
+        $HariIni = Carbon::now()->addHours(7)->startOfDay();
+        $skrg = Carbon::now()->addHours(7);
+        $jedaK = DB::table('waktus')
+        ->where('jenis_layanan','karantina')
+        ->pluck('jeda')
+        ->first();
+        $waktuAntriK=Carbon::parse($skrg)->subMinutes($jedaK);
+
+        $antrianK = DB::table('antrians')
+            ->where('jenis_layanan', 'karantina')
+            ->where('tanggal_antrian', '>', $HariIni)
+            ->select('no_antrian', 'no_ppk', 'tanggal_antrian', 'email', 'status')
+            ->get();
+        $jumlahK = count($antrianK);
+
+        $nextantriK = DB::table('antrians')
+        ->where('jenis_layanan', 'karantina')
+        ->where('tanggal_antrian', '>', $skrg)
+        ->orderBy('id', 'asc')
+        ->pluck('no_antrian')
+        ->first();
+
+        if(isset($nextantriK)){
+            $panggilK = DB::table('antrians')
+            ->where('jenis_layanan', 'karantina')
+            ->where('tanggal_antrian', '<', $skrg)
+            ->where('tanggal_antrian', '>', $HariIni)
+            ->orderBy('id', 'desc')
+            ->pluck('no_antrian')
+            ->first();
+        }else{
+            $panggilK = DB::table('antrians')
+            ->where('jenis_layanan', 'karantina')
+            ->where('tanggal_antrian', '>', $waktuAntriK)
+            ->orderBy('id', 'desc')
+            ->pluck('no_antrian')
+            ->first();
+        }
+
+        $antrisisaK = DB::table('antrians')
+        ->where('jenis_layanan', 'karantina')
+        ->where('tanggal_antrian', '>', $skrg)
+        ->select('no_antrian', 'no_ppk', 'tanggal_antrian', 'email', 'status')
+        ->get();
+        $sisaK = count($antrisisaK);
+
+        $jedaM = DB::table('waktus')
+        ->where('jenis_layanan','mutu')
+        ->pluck('jeda')
+        ->first();
+        $waktuAntriM=Carbon::parse($skrg)->subMinutes($jedaM);
+
+        $antrianM = DB::table('antrians')
+            ->where('jenis_layanan', 'mutu')
+            ->where('tanggal_antrian', '>', $HariIni)
+            ->select('no_antrian', 'no_ppk', 'tanggal_antrian', 'email', 'status')
+            ->get();
+        $jumlahM = count($antrianM);
+
+        $nextantriM = DB::table('antrians')
+        ->where('jenis_layanan', 'mutu')
+        ->where('tanggal_antrian', '>', $skrg)
+        ->orderBy('id', 'asc')
+        ->pluck('no_antrian')
+        ->first();
+
+        if(isset($nextantriM)){
+            $panggilM = DB::table('antrians')
+            ->where('jenis_layanan', 'mutu')
+            ->where('tanggal_antrian', '<', $skrg)
+            ->where('tanggal_antrian', '>', $HariIni)
+            ->orderBy('id', 'desc')
+            ->pluck('no_antrian')
+            ->first();
+        }else{
+            $panggilM = DB::table('antrians')
+            ->where('jenis_layanan', 'mutu')
+            ->where('tanggal_antrian', '>', $waktuAntriM)
+            ->orderBy('id', 'desc')
+            ->pluck('no_antrian')
+            ->first();
+        }
+
+        $antrisisaM = DB::table('antrians')
+        ->where('jenis_layanan', 'mutu')
+        ->where('tanggal_antrian', '>', $skrg)
+        ->select('no_antrian', 'no_ppk', 'tanggal_antrian', 'email', 'status')
+        ->get();
+        $sisaM = count($antrisisaM);
+
+        $jedaCS = DB::table('waktus')
+        ->where('jenis_layanan','cs')
+        ->pluck('jeda')
+        ->first();
+        $waktuAntriCS=Carbon::parse($skrg)->subMinutes($jedaCS);
+
+        $antrianCS = DB::table('antrians')
+            ->where('jenis_layanan', 'cs')
+            ->where('tanggal_antrian', '>', $HariIni)
+            ->select('no_antrian', 'no_ppk', 'tanggal_antrian', 'email', 'status')
+            ->get();
+        $jumlahCS = count($antrianCS);
+
+        $nextantriCS = DB::table('antrians')
+        ->where('jenis_layanan', 'cs')
+        ->where('tanggal_antrian', '>', $skrg)
+        ->orderBy('id', 'asc')
+        ->pluck('no_antrian')
+        ->first();
+
+        if(isset($nextantriCS)){
+            $panggilCS = DB::table('antrians')
+            ->where('jenis_layanan', 'cs')
+            ->where('tanggal_antrian', '<', $skrg)
+            ->where('tanggal_antrian', '>', $HariIni)
+            ->orderBy('id', 'desc')
+            ->pluck('no_antrian')
+            ->first();
+        }else{
+            $panggilCS = DB::table('antrians')
+            ->where('jenis_layanan', 'mutu')
+            ->where('tanggal_antrian', '>', $waktuAntriCS)
+            ->orderBy('id', 'desc')
+            ->pluck('no_antrian')
+            ->first();
+        }
+
+        $antrisisaCS = DB::table('antrians')
+        ->where('jenis_layanan', 'cs')
+        ->where('tanggal_antrian', '>', $skrg)
+        ->select('no_antrian', 'no_ppk', 'tanggal_antrian', 'email', 'status')
+        ->get();
+        $sisaCS = count($antrisisaCS);
+
         return view('dashboard.admin', [
             'title' => 'Dashboard',
-            'active' => 'admin'
+            'active' => 'admin',
+            'jumlahK' => $jumlahK,
+            'panggilK' => $panggilK,
+            'nextantriK' => $nextantriK,
+            'sisaK' => $sisaK,
+            'jumlahM' => $jumlahM,
+            'panggilM' => $panggilM,
+            'nextantriM' => $nextantriM,
+            'sisaM' => $sisaM,
+            'jumlahCS' => $jumlahCS,
+            'panggilCS' => $panggilCS,
+            'nextantriCS' => $nextantriCS,
+            'sisaCS' => $sisaCS,
         ]);
     }
 }
